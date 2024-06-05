518af1199ed7c36177349dff4b2090c5eb66467c
abap34 <abap0002@gmail.com>
2024-06-05 23:51:10 +0900

    CORS許可

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,9 +1,23 @@
 # fast api and uvicorn
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
