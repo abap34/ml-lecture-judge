2f01543c5f845c9e90d08df0354c5074bc137a58
abap34 <abap0002@gmail.com>
2024-06-06 21:36:34 +0900

    実行のフレームワーク作った

---
+++ backend/src/executor.py
@@ -0,0 +1,88 @@
+import argparse
+from dataclasses import dataclass
+from pathlib import Path
+import subprocess
+import os
+
+import datetime
+from typing import Literal
+from dataclasses import dataclass
+import json
+from pathlib import Path
+
+@dataclass
+class Config:
+    code_file: Path
+    input_file: Path
+    time: float
+    memory: float
+
+
+@dataclass
+class Result:
+    stdout: str
+    stderr: str
+    status: Literal["OK", "RE", "TLE", "MLE", "IE"]
+    time: float
+    memory: float
+
+    def to_json(self):
+        return json.dumps(self.__dict__)
+
+
+def main(config: Config) -> Result:
+    cmd = ["python", str(config.code_file)]
+    try:
+        result = subprocess.run(
+            cmd,
+            # ms で渡されるので s に変換
+            timeout=config.time * 1000,
+            input=config.input_file.read_text(),
+            check=True,  # CalledProcessError を発生させる
+            capture_output=True,
+            text=True,
+            encoding="utf-8",
+        )
+
+    except subprocess.TimeoutExpired as e:
+        return Result(
+            stdout="",
+            stderr="",
+            status="TLE",
+            time=e.timeout,
+            memory=0.0,
+        )
+    except subprocess.CalledProcessError as e:
+        return Result(
+            stdout="",
+            stderr=e.stderr,
+            status="RE",
+            time=0.0,
+            memory=0.0,
+        )
+
+    return Result(
+        stdout=result.stdout,
+        stderr=result.stderr,
+        status="OK",
+        time=0.0,
+        memory=0.0,
+    )
+
+
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("--code", type=str, required=True)
+    argparser.add_argument("--input", type=str, required=True)
+    argparser.add_argument("--time", type=float, default=1.0)
+    args = argparser.parse_args()
+
+    config = Config(
+        code_file=Path(args.code),
+        input_file=Path(args.input),
+        time=args.time,
+        memory=256.0,
+    )
+
+    result = main(config)
+    print(result.to_json())
--- backend/src/judge/tasks.py
+++ backend/src/judge/tasks.py
@@ -1,27 +1,138 @@
 from celery import Celery
 import os
-import time
-import sqlite3
+import json
+import docker
 
-# RedisブローカーのURLを取得
+
 REDIS_URL = os.getenv("REDIS_URL", "redis://redis:6379/0")
 
-app = Celery('tasks', broker=REDIS_URL, backend=REDIS_URL)
+app = Celery("tasks", broker=REDIS_URL, backend=REDIS_URL)
+
+import docker
+
+from dataclasses import dataclass
+
+from datetime import datetime
+
+
+class ExecutionResult:
+    def __init__(
+        self,
+        stdout: str,
+        stderr: str,
+        status: str,
+        time: float,
+        memory: float,
+        finish_datetime: datetime,
+    ):
+        self.stdout = stdout
+        self.stderr = stderr
+        self.status = status
+        self.time = time
+        self.memory = memory
+        self.finish_datetime = finish_datetime
+
+    @staticmethod
+    def from_json(data: dict):
+        return ExecutionResult(
+            stdout=data["stdout"],
+            stderr=data["stderr"],
+            status=data["status"],
+            time=data["time"],
+            memory=data["memory"],
+            finish_datetime=datetime.now(),
+        )
+
+    def to_json(self):
+        d = {
+            "stdout": self.stdout,
+            "stderr": self.stderr,
+            "status": self.status,
+            "time": self.time,
+            "memory": self.memory,
+            "finish_datetime": self.finish_datetime.isoformat(),
+        }
+
+        return json.dumps(d)
+
+
+def build_command(code, input_data, time):
+    code = code.replace('"', '\\"')
+    input_data = input_data.replace('"', '\\"')
+    # /bin/sh を挟まないと && が使えない
+    return f"/bin/sh -c \"echo '{code}' > code.py && echo '{input_data}' > input.txt && python /app/executor.py --code code.py --input input.txt --time {time}\""
+
+
+def run(code: str, input_data: str, time: float, memory: float) -> ExecutionResult:
+    client = docker.from_env()
+    try:
+        log = client.containers.run(
+            image="executor_image",
+            command=build_command(code, input_data, time),
+            cpu_period=int(time * 100000),
+            mem_limit=f"{memory}m",
+            pids_limit=4,
+            stdout=True,
+            stderr=True,
+        )
+
+        stdout = log.decode("utf-8")
+
+        try:
+            return ExecutionResult.from_json(json.loads(stdout))
+
+        except json.JSONDecodeError:
+            return ExecutionResult(
+                stdout="",
+                stderr="Failed to parse the result. value: " + stdout,
+                status="IE",
+                time=0.0,
+                memory=0.0,
+                finish_datetime=datetime.now(),
+            )
+
+    # 時間制限
+    except docker.errors.ContainerError as e:
+        return ExecutionResult(
+            stdout=e.stderr.decode("utf-8"),
+            stderr="",
+            status="TLE",
+            time=0.0,
+            memory=0.0,
+            finish_datetime=datetime.now(),
+        )
+
+    # メモリ制限
+    except docker.errors.APIError as e:
+        return ExecutionResult(
+            stdout="",
+            stderr=str(e),
+            status="MLE",
+            time=0.0,
+            memory=0.0,
+            finish_datetime=datetime.now(),
+        )
+    
+    #　予期せぬそのほかのエラー (ランタイムエラーは含まれない！. REは executor.py で処理される)
+    except docker.errors.DockerException as e:
+        return ExecutionResult(
+            stdout="",
+            stderr=str(e),
+            status="IE",
+            time=0.0,
+            memory=0.0,
+            finish_datetime=datetime.now(),
+        )
+    finally:
+        client.close()
+
 
 @app.task(name="tasks.evaluate_code")
-def evaluate_code(code, submission_time):
-    print("Evaluating code...")
-    time.sleep(5)
-    print("Code evaluated.")
-    return eval(code), submission_time
+def evaluate_code(code: str, submission_time: str):
+    # 20s
+    result = run(code, input_data="hogehoge", time=20000.0, memory=256.0)
 
-def update_submission_result(submission_id, result):
-    conn = sqlite3.connect('/app/data/database.db')
-    c = conn.cursor()
-    c.execute('''
-        UPDATE submissions
-        SET result = ?
-        WHERE id = ?
-    ''', (result, submission_id))
-    conn.commit()
-    conn.close()
+    return {
+        "submission_time": submission_time,
+        "result": result.to_json(),
+    }
