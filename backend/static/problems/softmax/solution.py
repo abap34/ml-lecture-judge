3b5209e8bb6c04696290d6bc6e8bbfe9dbcdba66
rusty <codingtrain9@gmail.com>
2024-06-14 16:01:29 +0900

    安定なsoftmax

--- backend/static/problems/softmax/solution.py
+++ backend/static/problems/softmax/solution.py
@@ -6,7 +6,8 @@ x = np.array(list(map(float, input().split())))
 
 # ソフトマックス関数の定義
 def softmax(x):
-    return np.exp(x) / np.sum(np.exp(x))
+    exp_x = np.exp(x - np.max(x))
+    return exp_x / np.sum(exp_x)
 
 # 出力
 print(*softmax(x))
