7c834fba09273d1f67eb47063954bb2d9cc0a936
abap34 <abap0002@gmail.com>
2024-06-18 17:25:19 +0900

    ログインボタン追加

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import { Typography, Box, Drawer, AppBar, Toolbar, CssBaseline, IconButton } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link, useNavigate } from 'react-router-dom';
+import { Typography, Box, Drawer, AppBar, Toolbar, CssBaseline, IconButton, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Button } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
@@ -8,6 +8,7 @@ import ProblemList from './components/ProblemList';
 import ProblemDetail from './components/ProblemDetail';
 import Welcome from './components/Welcome';
 import SubmissionResult from './components/SubmissionResult';
+import axios from 'axios';
 
 // カスタムテーマの作成
 const theme = createTheme({
@@ -37,9 +38,10 @@ const theme = createTheme({
           borderRadius: '8px',
           textTransform: 'none',
           fontSize: '16px',
-          backgroundColor: '#ff9854',
+          backgroundColor: '#FFFFFF', 
+          color: '#ff9854', 
           '&:hover': {
-            backgroundColor: '#FFE0B2',
+            backgroundColor: '#FFE0B2', 
           },
         },
       },
@@ -54,63 +56,138 @@ const theme = createTheme({
   },
 });
 
-function App() {
+
+const AppContent = () => {
   const [isDrawerOpen, setDrawerOpen] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [openDialog, setOpenDialog] = useState(false);
+
+  useEffect(() => {
+    const checkLoginStatus = async () => {
+      try {
+        const response = await axios.get('http://localhost:8000/login_status', { withCredentials: true });
+        if (response.data.logged_in) {
+          console.log('User is logged in');
+          setIsLoggedIn(true);
+          getUserInfo();
+        } else {
+          console.log('User is not logged in');
+          setOpenDialog(true);
+        }
+      } catch (error) {
+        console.error('Error checking login status:', error);
+      }
+    };
+
+    checkLoginStatus();
+  }, []);
+
+  const getUserInfo = async () => {
+    try {
+      // get bearer token. get from cookie
+      const token = document.cookie.split('=')[1];
+      console.log('token:', token);
+      
+      const response = await axios.get('http://localhost:8000/userinfo', { withCredentials: true });
+      console.log('Logged in user:', response.data.userinfo.name);
+    } catch (error) {
+      console.error('Error getting user info:', error);
+    }
+  };
+
+  const handleLogin = async () => {
+    try {
+      window.location.href = 'http://localhost:8000/login';
+    } catch (error) {
+      console.error('Error during login:', error);
+    }
+  };
 
   const toggleDrawer = () => {
     setDrawerOpen(!isDrawerOpen);
   };
 
   return (
+    <>
+      <CssBaseline />
+      <AppBar position="fixed">
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={toggleDrawer}
+            sx={{ mr: 2, ...(isDrawerOpen && { display: 'none' }) }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            <Link to="/" style={{ textDecoration: 'none', color: 'white' }}>ml-lecture online judge</Link>
+          </Typography>
+          <Typography variant="h6" noWrap sx={{ flexGrow: 1 }} />
+           Logged in as: {isLoggedIn ? 'testuser' : 'Guest'}
+        </Toolbar>
+      </AppBar>
+      <Box sx={{ display: 'flex' }}>
+        <Drawer
+          variant="temporary"
+          open={isDrawerOpen}
+          onClose={toggleDrawer}
+          sx={{
+            '& .MuiDrawer-paper': {
+              width: 240,
+            },
+          }}
+        >
+          <Toolbar>
+            <IconButton onClick={toggleDrawer}>
+              <CloseIcon />
+            </IconButton>
+          </Toolbar>
+          <ProblemList onClose={toggleDrawer} />
+        </Drawer>
+        <Box
+          component="main"
+          sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
+        >
+          <Toolbar />
+          <Routes>
+            <Route path="/problems/:problemName" element={<ProblemDetail />} />
+            <Route path="/result/:taskId" element={<SubmissionResult />} />
+            <Route path="/" element={<Welcome />} />
+          </Routes>
+        </Box>
+      </Box>
+      <Dialog
+        open={openDialog}
+        onClose={() => setOpenDialog(false)}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"ログインが必要です"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            問題の閲覧・提出はログインが必要です。ログインしますか？
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setOpenDialog(false)}  autoFocus>
+            キャンセル
+          </Button>
+          <Button onClick={handleLogin}  autoFocus>
+            ログイン
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+}
+
+function App() {
+  return (
     <ThemeProvider theme={theme}>
       <Router>
-        <CssBaseline />
-        <AppBar position="fixed">
-          <Toolbar>
-            <IconButton
-              color="inherit"
-              aria-label="open drawer"
-              edge="start"
-              onClick={toggleDrawer}
-              sx={{ mr: 2, ...(isDrawerOpen && { display: 'none' }) }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" noWrap>
-              <Link to="/" style={{ textDecoration: 'none', color: 'white' }}> ml-lecture online judge </Link>
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <Box sx={{ display: 'flex' }}>
-          <Drawer
-            variant="temporary"
-            open={isDrawerOpen}
-            onClose={toggleDrawer}
-            sx={{
-              '& .MuiDrawer-paper': {
-                width: 240,
-              },
-            }}
-          >
-            <Toolbar>
-              <IconButton onClick={toggleDrawer}>
-                <CloseIcon />
-              </IconButton>
-            </Toolbar>
-            <ProblemList onClose={toggleDrawer} />
-          </Drawer>
-          <Box
-            component="main"
-            sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
-          >
-            <Toolbar />
-            <Routes>
-              <Route path="/problems/:problemName" element={<ProblemDetail />} />
-              <Route path="/result/:taskId" element={<SubmissionResult />} />
-              <Route path="/" element={<Welcome />} />
-            </Routes>
-          </Box>
-        </Box>
+        <AppContent />
       </Router>
     </ThemeProvider>
   );
