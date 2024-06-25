e4499fc847232b2dd0bbfe8042ea7be7a18f857c
abap34 <abap0002@gmail.com>
2024-06-26 01:26:20 +0900

    ユーザにチームid追加

--- backend/src/models.py
+++ backend/src/models.py
@@ -20,7 +20,7 @@ class User(Base):
     id = Column(String, primary_key=True, index=True)
     created_at = Column(TIMESTAMP, default=jst_now)
     icon_url = Column(String, nullable=True)
-
+    team_id = Column(Integer, ForeignKey("teams.id"), nullable=True)
 
 class Team(Base):
     __tablename__ = "teams"
