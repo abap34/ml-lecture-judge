b898983070b6898b8ab17d3d5b00a8cfe9d9eff5
abap34 <abap0002@gmail.com>
2024-06-28 23:45:44 +0900

    shape error 直した

--- backend/static/problems/autograd-practice-3/solution.py
+++ backend/static/problems/autograd-practice-3/solution.py
@@ -1,8 +1,7 @@
 import torch
 
-# 問題3
 W = torch.tensor([[1.0, 2.0], [2.0, 1.0]])
-x1 = torch.tensor([[1.0, 2.0]], requires_grad=True)
+x1 = torch.tensor([1.0, 2.0], requires_grad=True)
 
 y = torch.matmul(torch.matmul(x1, W), x1)
 y.backward()
